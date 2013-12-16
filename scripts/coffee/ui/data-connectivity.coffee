f96a7dfb62af8178bdbbbeca2a70d3b1041123e3
Kurtis Kemple <kurtiskemple@gmail.com>
2013-12-16 17:25:25 -0500

    updated data-connectivity.coffee to use d3.js force layout for tree example

--- scripts/coffee/ui/data-connectivity.coffee
+++ scripts/coffee/ui/data-connectivity.coffee
@@ -1,15 +1,102 @@
 Union = () ->
     @data = []
+    @links = []
     i = 0
-    while i < 10
+    while i < 100
         @data.push
             id: i + 1
             parent: i + 1
             children: []
+            link: i
             hasParent: false
-            color: '#'+Math.floor(Math.random()*16777215).toString(16) # generate random color
+            color: '#'+Math.floor(Math.random()*16777215).toString(16) 
+
+        @links.push
+            source: i
+            target: i
+            value: 5
         ++i
+
+    @init()
     return @
+
+Union::init = () ->
+    width = $('#tree-graph').width()
+    height = 400
+
+    @force = d3.layout.force()
+                    .charge(-60)
+                    .linkDistance(25)
+                    .size([width, height])
+
+    @svg = d3.select('#tree-graph').append('svg')
+                    .attr('width', width)
+                    .attr('height', height)
+
+    @force
+        .nodes( @data )
+        .links( @links )
+        .start()
+
+    @link = @svg.selectAll(".link")
+                .data( @links )
+                .enter()
+                .append("line")
+                .attr("class", "link")
+                .style("stroke-width", (d) -> return Math.sqrt( d.value ) )
+
+    @node = @svg.selectAll( ".node" )
+                    .data( @data )
+                    .enter()
+                    .append( "circle" )
+                    .attr( "class", "node" )
+                    .attr( "r", 7.5 )
+                    .attr( 'cx', (d) -> return Math.floor( Math.random() * ( width - 10 ) ) )
+                    .attr( 'cy', (d) -> return Math.floor( Math.random() * ( height - 10 ) ) )
+                    .style("fill", (d) -> return d.color )
+                    .call(@force.drag)
+
+    @events()
+
+Union::getUnique = () ->
+    prevIndexes = []
+
+    unique = Math.floor( Math.random() * @data.length )
+
+    unless _.indexOf( prevIndexes, unique ) > 0
+        return unique
+    else
+        return @getUnique()
+
+Union::events = () ->
+    self = @
+
+    @force.on "tick", () ->
+            self.link
+                .attr("x1", (d) -> return d.source.x )
+                .attr("y1", (d) -> return d.source.y )
+                .attr("x2", (d) -> return d.target.x )
+                .attr("y2", (d) -> return d.target.y )
+
+            self.node
+                .attr("cx", (d) -> return d.x )
+                .attr("cy", (d) -> return d.y )
+
+    $('.auto').on 'click', (e) ->
+            id1 = self.getUnique()
+            id2 = self.getUnique()
+            node1 = self.getNode( 'id', id1 )
+            node2 = self.getNode( 'id', id2 )
+            self.union( node1, node2 )
+            e.preventDefault()
+
+    $('.join').on 'click', (e) ->
+            selector1 = parseInt( $( '#num1' ).val(), 10 )
+            selector2 = parseInt( $( '#num2' ).val(), 10 )
+            node1 = self.getNode( 'id', selector1 )
+            node2 = self.getNode( 'id', selector2 )
+            self.union(  node1,  node2 )
+            e.preventDefault()
 
 Union::root = ( node ) ->
     self = @
@@ -23,26 +110,34 @@ Union::root = ( node ) ->
 Union::union = ( node1, node2 ) ->
     self = @
     unless @connected node1, node2
-        if @weight( node1 ) > @weight( node2 )
-            child = @root node1
-            parent = @root node2
+        root1 = @root( node1 )
+        root2 = @root( node2 )
+        weight1 = @weight( root1 )
+        weight2 = @weight( root2 )
+
+        if weight1 > weight2
+            child = root2
+            parent = root1
+
+        else if weight1 == weight2
+            child = root2
+            parent = root1
 
         else
-            child = @root node2
-            parent = @root node1
+            child = root1
+            parent = root2
 
         child.hasParent = true
         child.parent = parent.id
-        @setColors(child, parent.color)
+        @links[child.link].target = parent.link
+        @setColors child, parent.color
         parent.children.push child.id
 
-        d3.select('#tree-graph').selectAll('rect')
-            .data(@data)
-            .transition()
-            .delay(300)
-            .attr('fill', (d) ->
-                return d.color
-            )
+        @svg.selectAll('circle')
+                .data(@data)
+                .attr( 'fill', (d) -> return d.color )
+        @force.nodes(u.data)
+        @force.start()
 
         return parent
     else
@@ -75,41 +170,7 @@ Union::getNode = ( key, value ) ->
 
 Union::connected = ( node1, node2 ) ->
     return @root( node1 ) == @root( node2 )
-
-Union::auto = () ->
-    loops = Math.floor( Math.random() * @data.length )
-    i = 0
-
-    while i < loops
-        @union( @getNode( 'id', Math.floor( Math.random() * @data.length ) ), @getNode( 'id', Math.floor( Math.random() * @data.length ) ) )
-        ++i
-    console.log 'Complete'
+    
 
 window.u = new Union
 
-width = $('#tree-graph').width()
-height = $('#tree-graph').height()
-
-d3.select('#tree-graph').selectAll('rect')
-    .data(u.data)
-    .enter()
-    .append('rect')
-    .attr('height', height)
-    .attr("width", width / u.data.length - 1)
-    .attr('y', 0)
-    .attr("x", (d, i) ->
-        return i * (width/ u.data.length);
-    )
-    .attr('fill', (d) ->
-        return d.color
-    )
-
-$('.auto').on 'click', (e) ->
-    u.auto()
-    e.preventDefault()
-
-$('.join').on 'click', (e) ->
-    node1 = u.getNode( 'id', $( 'input[name="num1"]' ).val() )
-    node2 = u.getNode( 'id', $( 'input[name="num2"]' ).val() )
-    u.union(  node1,  node2 )
-    e.preventDefault()
--- scripts/js/ui/data-connectivity.js
+++ scripts/js/ui/data-connectivity.js
@@ -1,21 +1,99 @@
 (function() {
-  var Union, height, width;
+  var Union;
 
   Union = function() {
     var i;
     this.data = [];
+    this.links = [];
     i = 0;
-    while (i < 10) {
+    while (i < 100) {
       this.data.push({
         id: i + 1,
         parent: i + 1,
         children: [],
+        link: i,
         hasParent: false,
         color: '#' + Math.floor(Math.random() * 16777215).toString(16)
       });
+      this.links.push({
+        source: i,
+        target: i,
+        value: 5
+      });
       ++i;
     }
+    this.init();
     return this;
+  };
+
+  Union.prototype.init = function() {
+    var height, width;
+    width = $('#tree-graph').width();
+    height = 400;
+    this.force = d3.layout.force().charge(-60).linkDistance(25).size([width, height]);
+    this.svg = d3.select('#tree-graph').append('svg').attr('width', width).attr('height', height);
+    this.force.nodes(this.data).links(this.links).start();
+    this.link = this.svg.selectAll(".link").data(this.links).enter().append("line").attr("class", "link").style("stroke-width", function(d) {
+      return Math.sqrt(d.value);
+    });
+    this.node = this.svg.selectAll(".node").data(this.data).enter().append("circle").attr("class", "node").attr("r", 7.5).attr('cx', function(d) {
+      return Math.floor(Math.random() * (width - 10));
+    }).attr('cy', function(d) {
+      return Math.floor(Math.random() * (height - 10));
+    }).style("fill", function(d) {
+      return d.color;
+    }).call(this.force.drag);
+    return this.events();
+  };
+
+  Union.prototype.getUnique = function() {
+    var prevIndexes, unique;
+    prevIndexes = [];
+    unique = Math.floor(Math.random() * this.data.length);
+    if (!(_.indexOf(prevIndexes, unique) > 0)) {
+      return unique;
+    } else {
+      return this.getUnique();
+    }
+  };
+
+  Union.prototype.events = function() {
+    var self;
+    self = this;
+    this.force.on("tick", function() {
+      self.link.attr("x1", function(d) {
+        return d.source.x;
+      }).attr("y1", function(d) {
+        return d.source.y;
+      }).attr("x2", function(d) {
+        return d.target.x;
+      }).attr("y2", function(d) {
+        return d.target.y;
+      });
+      return self.node.attr("cx", function(d) {
+        return d.x;
+      }).attr("cy", function(d) {
+        return d.y;
+      });
+    });
+    $('.auto').on('click', function(e) {
+      var id1, id2, node1, node2;
+      id1 = self.getUnique();
+      id2 = self.getUnique();
+      node1 = self.getNode('id', id1);
+      node2 = self.getNode('id', id2);
+      self.union(node1, node2);
+      return e.preventDefault();
+    });
+    return $('.join').on('click', function(e) {
+      var node1, node2, selector1, selector2;
+      selector1 = parseInt($('#num1').val(), 10);
+      selector2 = parseInt($('#num2').val(), 10);
+      node1 = self.getNode('id', selector1);
+      node2 = self.getNode('id', selector2);
+      self.union(node1, node2);
+      return e.preventDefault();
+    });
   };
 
   Union.prototype.root = function(node) {
@@ -29,23 +107,33 @@
   };
 
   Union.prototype.union = function(node1, node2) {
-    var child, parent, self;
+    var child, parent, root1, root2, self, weight1, weight2;
     self = this;
     if (!this.connected(node1, node2)) {
-      if (this.weight(node1) > this.weight(node2)) {
-        child = this.root(node1);
-        parent = this.root(node2);
+      root1 = this.root(node1);
+      root2 = this.root(node2);
+      weight1 = this.weight(root1);
+      weight2 = this.weight(root2);
+      if (weight1 > weight2) {
+        child = root2;
+        parent = root1;
+      } else if (weight1 === weight2) {
+        child = root2;
+        parent = root1;
       } else {
-        child = this.root(node2);
-        parent = this.root(node1);
+        child = root1;
+        parent = root2;
       }
       child.hasParent = true;
       child.parent = parent.id;
+      this.links[child.link].target = parent.link;
       this.setColors(child, parent.color);
       parent.children.push(child.id);
-      d3.select('#tree-graph').selectAll('rect').data(this.data).transition().delay(300).attr('fill', function(d) {
+      this.svg.selectAll('circle').data(this.data).attr('fill', function(d) {
         return d.color;
       });
+      this.force.nodes(u.data);
+      this.force.start();
       return parent;
     } else {
       return console.log('Already connected');
@@ -90,40 +178,6 @@
     return this.root(node1) === this.root(node2);
   };
 
-  Union.prototype.auto = function() {
-    var i, loops;
-    loops = Math.floor(Math.random() * this.data.length);
-    i = 0;
-    while (i < loops) {
-      this.union(this.getNode('id', Math.floor(Math.random() * this.data.length)), this.getNode('id', Math.floor(Math.random() * this.data.length)));
-      ++i;
-    }
-    return console.log('Complete');
-  };
-
   window.u = new Union;
-
-  width = $('#tree-graph').width();
-
-  height = $('#tree-graph').height();
-
-  d3.select('#tree-graph').selectAll('rect').data(u.data).enter().append('rect').attr('height', height).attr("width", width / u.data.length - 1).attr('y', 0).attr("x", function(d, i) {
-    return i * (width / u.data.length);
-  }).attr('fill', function(d) {
-    return d.color;
-  });
-
-  $('.auto').on('click', function(e) {
-    u.auto();
-    return e.preventDefault();
-  });
-
-  $('.join').on('click', function(e) {
-    var node1, node2;
-    node1 = u.getNode('id', $('input[name="num1"]').val());
-    node2 = u.getNode('id', $('input[name="num2"]').val());
-    u.union(node1, node2);
-    return e.preventDefault();
-  });
 
 }).call(this);
